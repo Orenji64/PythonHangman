9b97db0608d770144abff169d12c3029f7a894b7
Orenji64 <71147398+Orenji64@users.noreply.github.com>
2023-05-01 14:52:13 +0100

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,13 +1,14 @@
 import urllib.request
 import random
 import webbrowser
+import os
 
 response = urllib.request.urlopen("https://www.mit.edu/~ecprice/wordlist.10000")
 plainlist = response.read().decode()
 words = plainlist.splitlines()
 wordHistory=[]
 
-def clear():print("\n"*100)
+def clear():print("\n"*100);os.system('cls')
 
 def man(f):
     frames= [
@@ -23,35 +24,43 @@ def man(f):
     ]
     return frames[f]
 
-def display(fails,word):
-    #clear()
-    print(man(fails))
-    print("Word: "+''.join(displayWord)+"\nFails: "+str(fails))
-
-def win():
+def display(fails,letters,word):
     clear()
-    print("Success")
+    print(man(fails)+"\n")
+    print("Word: "+''.join(displayWord))
+    print("Fails: ",end='')
+    for i in letters:print(i.upper(),end=', ')
+    print("\n")
+
+def win(currentWord):
+    clear()
+    print("Success! The word was "+currentWord)
+    input()
+def lose(ans):
+    #clear()
+    print("You lost. The word was "+ans)
     input()
 
 while True:
     while True:
-        currentWord=random.choice(words)
+        currentWord=random.choice(words).lower()
         if len(currentWord)>3: break
     wordHistory.append(currentWord)
     wordLetters=[*currentWord]
 
     displayWord=list("_"*len(currentWord))
+    failedLetters=[]
     fails=0
 
     while fails<8:
         success=False
         # Display current status
-        display(fails,displayWord)
+        display(fails,failedLetters,displayWord)
         # Ask for a letter or word
-        attempt=input("Guess a letter or word: ")
+        attempt=input("Guess a letter or word: ").lower()
         if len(attempt)==1:
             # Check if letter is in the word and update displayWord
-            if attempt in displayWord:
+            if (attempt in failedLetters) or (attempt in displayWord):
                 print("Already guessed")
             for i in range(0,len(wordLetters)):
                 if wordLetters[i]==attempt:displayWord[i]=attempt;success=True
@@ -59,8 +68,10 @@ while True:
             # Check if word guess is correct
             if attempt==currentWord:
                 win();success=True
-        if success==False:fails+=1
+        if success==False:fails+=1;failedLetters.append(attempt)
         # Check if the word has been completed
-        if ''.join(displayWord)==currentWord:print("success");win()
-
+        if ''.join(displayWord)==currentWord:win();success=True
+    if won==True:win()
+    else: lose(currentWord)
     input()
+input()
